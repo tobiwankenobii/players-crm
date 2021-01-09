2a8c5e4ead69fd38c7a194d6840c99ef34101cde
tobiwankenobii <tobiv1337@gmail.com>
2021-01-09 11:33:57 +0100

    Add simple jwt

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -134,3 +134,8 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
+
+
+# DRF Configuration
+
+REST_FRAMEWORK = {"DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",)}
