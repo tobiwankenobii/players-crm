a6d0c3e2354881496f0568ea1bf19d244427bc05
tobiwankenobii <tobiv1337@gmail.com>
2021-01-09 12:39:15 +0100

    Change frontend to js format

---
+++ frontend/src/App.js
@@ -0,0 +1,5 @@
+const App = () => {
+    return <div>Working!</div>;
+};
+
+export default App;
--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App = () => {
-    return <h1>I'm working!</h1>;
-};
-
-export default App;
